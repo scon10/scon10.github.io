360cf05c897197a16a23b6c5564c68d124441c05
scon10 <115589793+scon10@users.noreply.github.com>
2025-11-07 15:29:47 -0500

    1st commit
    Initial Portfolio Site

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for nav links
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(anchor.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
